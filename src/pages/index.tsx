343a480e5dc88fbd6a0c4b8876e8e61c21e78fdb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-12-26 17:42:26 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,77 @@
-// crud todo with rfc
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Todo } from '../types';
-import React from 'react';
+import { Todo } from "@prisma/client";
+import axios from "axios";
+import { use, useEffect, useState } from "react";
 
 const Home: React.FC = () => {
-  const [title, setTitle] = useState<string>('');
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const fetchTodos = async () => {
-    try {
-      const response = await axios.get('/api/todos/read');
-      setTodos(response.data);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des tâches', error);
+    const [title, setTitle] = useState<string>('');
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const fetchTodos = async () => {
+    try{
+        const response= await axios.get('/api/todos/read');
+    setTodos(response.data);
+    console.log(response.data);
     }
-  };
+    catch (error) {
+        console.log("erreur lors de la récupération ",error);
+        }
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      await axios.post('/api/todos/create', { title });
-      setTitle('');
-      fetchTodos();
-    } catch (error) {
-      console.error("Erreur lors de l'ajout de la tâche", error);
     }
-  };
-
-  const handleToggleDone = async (todoId: number) => {
-    try {
-      await axios.put(`http://localhost:3000/api/todos/update`, { id: todoId, done: true  });
-      fetchTodos();
-    } catch (error) {
-      console.error("Erreur lors de la mise à jour de la tâche", error);
+    const createTodo = async () => {
+        try{
+            const response = await axios.post('/api/todos/create', {title});
+            setTitle('');
+            fetchTodos();
+        }
+        catch (error) {
+            console.log("erreur lors de la création ",error);
+            }
     }
-  };
+    const handleDone = async (todoId: number) => {
+        try{
+             await axios.put(`/api/todos/update`,{id: todoId,done: true});
+            fetchTodos();
+            console.log("ok");
+        }
+        catch (error) {
+            console.log("erreur lors de la mise à jour ",error);
+            }    
 
-  const handleDelete = async (todoId: number) => {
-    try {
-      await axios.delete(`/api/todos/delete?id=${todoId}`);
-      fetchTodos();
-    } catch (error) {
-      console.error('Erreur lors de la suppression de la tâche', error);
     }
-  };
+    const handleDelete = async (todoId: number) => {
+        if(confirm("Voulez-vous supprimer cette tache ?"))
+        {
+        try{
+             await axios.delete(`/api/todos/delete?id=${todoId}`);
+            fetchTodos();
+        }
+        catch (error) {
+            console.log("erreur lors de la suppression ",error);
+            }    
+        }
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+    };
+    useEffect(() => {  
+        fetchTodos();
+    }, []);
   
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          value={title} 
-          onChange={(e) => setTitle(e.target.value)} 
-          placeholder="Ajouter une tâche..." 
-        />
-        <button type="submit">Ajouter</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
-              {todo.title}
-            </span>
-            <button onClick={() => handleToggleDone(todo.id)}>
-              {todo.done ? 'Marquer comme non fait' : 'Marquer comme fait'}
-            </button>
-            <button onClick={() => handleDelete(todo.id)}>Supprimer</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+   <div>
+    <form onSubmit={createTodo}>
+        <input type="text" placeholder="Ajouter une tache" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button type="submit">ajouter</button>
 
+    </form>
+    <ul>
+        {todos.map((todo) => (
+            <li key={todo.id}>
+                <input type="checkbox" checked={todo.done} onChange={() => handleDone(todo.id)} />
+                <span>{todo.title}</span>
+                <button onClick={() => handleDelete(todo.id)}>supprimer</button>
+            </li>
+        ))}
+    </ul>
+   </div>
+
+  );
+}
 export default Home;
